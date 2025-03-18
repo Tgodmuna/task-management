81229ff4b7c65a10c206fc6e9cef06b91e6eaae1
Aguthankgod <Aguthankgod@gmail.com>
2025-03-18 10:16:30 +0100

    Implement Authenticator component for user authentication; redirect to login if no token is found and display loading message.

--- src/components/auth/Authenticator.tsx
+++ src/components/auth/Authenticator.tsx
@@ -1,6 +1,35 @@
-const Authenticator = () => {
-  return (
-    <div>Authenticator</div>
-  )
-}
-export default Authenticator
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UseFetchToken } from "../hooks/UseFetchToken";
+import { AppContext } from "../../App";
+
+const Authenticator: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const token = UseFetchToken();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const appContext = useContext(AppContext);
+
+  useEffect(() => {
+    if (!token) {
+      console.log("No token found, redirecting...");
+      setIsAuthenticated(false);
+      setTimeout(() => {
+        navigate("/login", { replace: true });
+      }, 2000);
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [token, navigate, appContext]);
+
+  if (!isAuthenticated) {
+    return (
+      <h1 className="text-red-500 text-center animate-pulse m-auto text-4xl">
+        Not Authenticated, Redirecting...
+      </h1>
+    );
+  }
+
+  return <>{children}</>;
+};
+
+export default React.memo(Authenticator);
