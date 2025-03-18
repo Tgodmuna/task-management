b7450113f39057198969eed892213bb80b8d60be
Aguthankgod <Aguthankgod@gmail.com>
2025-03-18 10:14:26 +0100

    Enhance App component to manage user authentication and task fetching; integrate hooks for user data and environment variables, and improve context provider with task management functions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,108 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import type { AppContextType } from "./types";
+import type { AppContextType, TaskType, userType } from "./types";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/dashboard-components/Dashboard";
 import LoginForm from "./components/auth/Login";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import RegisterForm from "./components/auth/Register";
+import Authenticator from "./components/auth/Authenticator";
+import UseFetchUserData from "./components/hooks/UseFetchUser";
+import axios from "axios";
+import useEnvironmentUrls from "./components/hooks/UseEnvironmentVar";
+import { UseFetchToken } from "./components/hooks/UseFetchToken";
 //AppContext
 export const AppContext = React.createContext<null | AppContextType>(null);
 
 function App() {
   const [isloggedIn, SetIsloggedIn] = useState<boolean>(false);
+  const [userData, setUserData] = useState<null | userType>(UseFetchUserData());
+  const [tasks, setTasks] = useState<TaskType[] | []>([]);
+  const [isAddTaskOpen, setIsAddTaskOpen] = useState(false);
+  const { serverUrl } = useEnvironmentUrls();
+  const token = UseFetchToken();
+
+  //create task toggle function
+  const onCreateTask = () => {
+    setIsAddTaskOpen(!isAddTaskOpen);
+  };
+
+  const toggleLogin = (data: boolean) => {
+    console.log("called toggleLogin");
+    SetIsloggedIn(data);
+  };
+
+  useEffect(() => console.log({ isloggedIn }), [isloggedIn]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      try {
+        const response = await axios.get(`${serverUrl}/api/task/readTasks`, {
+          headers: {
+            "x-auth-token": token,
+          },
+        });
+        if (response.status === 401) {
+          console.log("access denied, no token provided");
+          toast.error(response.data);
+          return;
+        }
+
+        if (!response) {
+          toast.error("failed to fetch task");
+          console.log("failed to fetch task ");
+          return;
+        }
+        console.log(response);
+        setTasks(response.data?.tasks);
+      } catch (error) {
+        console.log(error);
+        toast.error(
+          `${(error as any)?.response?.data || (error as Error)?.message} || ${
+            (error as any).code
+          }` || "failed to fetch tasks.  Please try again."
+        );
+      }
+    }
+    //fetch task when user is logged in
+    isloggedIn && fetchTasks();
+  }, [isloggedIn, serverUrl, token]);
 
   return (
-    <AppContext.Provider value={{ isloggedIn, SetIsloggedIn }}>
+    <AppContext.Provider
+      value={{
+        userData,
+        isloggedIn,
+        toggleLogin,
+        setUserData,
+        setTasks,
+        tasks,
+        onCreateTask,
+        isAddTaskOpen,
+      }}>
       <div className="App">
         <ToastContainer />
-
         <Routes>
+          <Route
+            path="/"
+            element={<LoginForm />}
+          />
+
           {/* dashboard */}
           <Route
             path="/dashboard"
-            element={<Dashboard />}
+            element={
+              <Authenticator>
+                <Dashboard />
+              </Authenticator>
+            }
           />
-
           {/* auth */}
           <Route
             path="/login"
             element={<LoginForm />}
-          />  <Route
+          />
+          <Route
             path="/register"
             element={<RegisterForm />}
           />
