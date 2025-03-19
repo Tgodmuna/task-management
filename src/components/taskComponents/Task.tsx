055cdd0623edde9fbb19f41661c7513f934c468b
Aguthankgod <Aguthankgod@gmail.com>
2025-03-19 07:59:36 +0100

    Refactor Task component to remove dependency on AppContext; simplify state management for task lists and dropdown visibility.

--- src/components/taskComponents/Task.tsx
+++ src/components/taskComponents/Task.tsx
@@ -1,27 +1,18 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import type { TaskContextType, TaskType } from "../../types";
+import { createContext, useState } from "react";
+import type { TaskContextType } from "../../types";
 import TaskList from "./Task_List";
 import TaskInput from "./Task_input";
-import { AppContext } from "../../App";
 
 export const TaskContext = createContext<TaskContextType | null>(null);
 
 export const Task = () => {
-  const [tasks, setTasks] = useState<TaskType[] | []>([]);
   const [isTodoTaskListDropdownOpen, setIsTodoTaskListDropdownOpen] = useState<boolean>(true);
   const [inProgressTaskList, setInProgressTaskList] = useState(true);
   const [completedTaskList, setCompleteTaskList] = useState(true);
-  const appContext = useContext(AppContext);
-
-  useEffect(() => {
-    if (appContext && appContext?.tasks) setTasks(appContext?.tasks);
-  }, [appContext, appContext?.tasks]);
 
   return (
     <TaskContext.Provider
       value={{
-        tasks,
-        setTasks,
         isTaskListDropdownOpen: isTodoTaskListDropdownOpen,
         setIsTaskListDropdownOpen: setIsTodoTaskListDropdownOpen,
         completedTaskList,
@@ -29,7 +20,7 @@ export const Task = () => {
         setCompleteTaskList,
         setInProgressTaskList,
       }}>
-      <TaskList  />
+      <TaskList />
       <TaskInput />
     </TaskContext.Provider>
   );
